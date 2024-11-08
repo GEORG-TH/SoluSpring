1fe7908dbb6639009b224e14670a21b233cfc715
Georg Thierry <georg.delacruzacho@gmail.com>
2024-11-08 17:49:50 -0500

    Primer Commit

---
+++ TareasCasoI/src/main/java/com/example/TareasCasoI/TareasCasoIApplication.java
@@ -0,0 +1,13 @@
+package com.example.TareasCasoI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareasCasoIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareasCasoIApplication.class, args);
+	}
+
+}
---
+++ TareasCasoI/src/test/java/com/example/TareasCasoI/TareasCasoIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TareasCasoI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasCasoIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
